3c39b55f168c8f60f108f2ebd7391c34774536c0
Axel Martínez <ackzell@gmail.com>
2020-07-06 12:44:44 -0500

    Starting to integrate Vue support.

---
+++ client/TempComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    id="some-id"
+    class="my-class"
+  >
+    {{ message }} <br> {{ message2 }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: 'Hello YouTube!', message2: 'Subscribe' };
+  },
+  computed: {
+    myComputed() {
+      let undef = 4;
+      return undef;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ imports/ui/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>
+      {{ greeting }}
+    </p>
+
+    <temp-component />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello Meteor, from Vue',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
