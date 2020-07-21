540ad93dba374f26527792ef56d21b38f0f75423
Axel Martínez <ackzell@gmail.com>
2020-07-21 17:51:12 -0500

    Removing insecure, creating a meteor method to insert new items.

--- client/TempComponent.vue
+++ client/TempComponent.vue
@@ -15,20 +15,53 @@
         {{ item.name }} | ${{ item.price }}
       </li>
     </ul>
+
+    <form @submit.prevent="saveNewItem">
+      <label for="item-name">Name</label>
+      <input
+        id="item-name"
+        v-model="item.name"
+        type="text"
+      >
+      <br>
+      <label for="item-price">Price</label>
+      <input
+        id="item-price"
+        v-model="item.price"
+        type="text"
+      >
+      <button>Save</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { Meteor } from 'meteor/meteor';
 import { Items } from '/imports/api/items';
 
 export default {
   data() {
-    return { message: 'Hello YouTube!', message2: 'Subscribe' };
+    return {
+      message: 'Hello YouTube!',
+      message2: 'Subscribe',
+      item: { name: '', price: 0 },
+    };
   },
   computed: {
     myComputed() {
       let undef = 4;
       return undef;
+    },
+  },
+  methods: {
+    saveNewItem() {
+      Meteor.call('createItem', this.item, function(error, result) {
+        if (error) {
+          console.error('something went wrong', error);
+        } else {
+          console.info(result);
+        }
+      });
     },
   },
   meteor: {
--- imports/api/items.js
+++ imports/api/items.js
@@ -1,3 +1,10 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Items = new Mongo.Collection('items');
+
+Meteor.methods({
+  createItem(item) {
+    return Items.insert(item);
+  },
+});
