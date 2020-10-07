ee2b63f6bd891e2bc04fda01c655e847d2e78580
Johannes Klotzek <torox@gmx.de>
2020-10-07 17:11:56 +0200

    Added viewport in index.html

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-tabs>
-        <v-tab @click="$router.push('/')">
-          Home
-        </v-tab>
-        <v-tab @click="$router.push('/About')">
-          About
-        </v-tab>
-        <v-tab>Tab 3</v-tab>
-      </v-tabs>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <h1>
-          {{ greeting }}
-        </h1>
-      </v-container>
-      <v-container>
-        <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <!-- App.vue -->
+    <v-app>
+      <v-navigation-drawer app>
+        <!-- -->
+      </v-navigation-drawer>
+
+      <v-app-bar
+        class="mx-auto"
+        app
+      >
+        <v-tabs>
+          <v-tab @click="$router.push('/')">
+            Home
+          </v-tab>
+          <v-tab @click="$router.push('/About')">
+            About
+          </v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view />
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
