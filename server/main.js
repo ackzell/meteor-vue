91127755cc56ae33364a0d28ec8a7bd81eb10881
Johannes Klotzek <torox@gmx.de>
2021-10-11 17:07:24 +0200

    add Agenda Server

--- server/main.js
+++ server/main.js
@@ -1,7 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { MongoInternals } from 'meteor/mongo';
+import { Agenda } from "agenda/es";
 import '../imports/api/items';
 import '../imports/api/processes/processes';
 
+
+
+
 Meteor.startup(() => {
   // code to run on server at startup
+  const { db: mongo } = MongoInternals.defaultRemoteCollectionDriver().mongo;
+  const agenda = new Agenda({ mongo });
 });
